881b0d4fc6d9bb7be262e4eb99161b3ad23d0985
algorodev <me@algorodev.com>
2022-04-22 18:22:01 +0200

    [API] feature: create cors middleware

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-require('dotenv').config();
+require('dotenv').config()
 
-const MONGO_URL = process.env.DATABASE_URL;
+const MONGO_URL = process.env.DATABASE_URL
 
 mongoose.connect(MONGO_URL).then(() => console.log('---> SUCCESSFUL DATABASE CONNECTION'))
--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ require('dotenv').config()
 require('./config/dbconnect.js')
 
 const PORT = +process.env.PORT
+const cors = require('./middlewares/cors.js')
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors)
 
 app.listen(PORT, () => console.log('---> SERVER RUNNING ON PORT', PORT))
