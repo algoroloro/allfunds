476c1abf9dd0a7fce059584c95db28cd914e2f99
algorodev <me@algorodev.com>
2022-04-27 19:18:54 +0200

    [CLIENT] feature: install material and use it

---
+++ client/src/app/shared/material-components/material-components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatToolbarModule,
+  ]
+})
+export class MaterialComponentsModule { }
---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialComponentsModule } from './material-components/material-components.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialComponentsModule
+  ],
+  exports: [
+    CommonModule,
+    MaterialComponentsModule
+  ]
+})
+export class SharedModule { }
